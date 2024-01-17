48bd2abf0b2b242b75f4d211e5df433f18087c5f
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-01-17 14:44:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function firstNonRepeatedChar(str) {
+function firstNonRepeatedChar(s) {
  // Write your code here
-// }
+	
+	for(let i=0 ; i<s.length ;i++){
+		let cnt = 0;
+		while(i+1<s.length && s.charAt(i)===s.charAt(i+1)){
+			cnt++;
+			i++;
+		}
+		if(cnt===0){
+			return s.charAt(i);
+		}
+	}
+	return;
+ }
 // const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
